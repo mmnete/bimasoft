6115a2b7ee84d6974ecf488917f2501f2cacca15
mmnete <mnetemohamed@gmail.com>
2025-02-23 00:30:16 -0800

    finalize

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://production-api.example.com/api',
+  apiUrl: 'https://bima-soft-backend-80063215530d.herokuapp.com/api',
   apiKey: '' // API key will be injected via environment variables
 };
